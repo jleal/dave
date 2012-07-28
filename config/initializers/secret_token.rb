c116ff1b618a325f44075122f1fdec1d48d63f68
Juan Leal <sellingangle@hotmail.com>
2012-07-28 18:40:06 -0500

    Created new dave app. Huzzahgit status!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dave::Application.config.secret_token = 'e300a61a11663617cf430804649708273dc1e7389752650f52e1ed14013290a08e000867a1fcbf687c9384ef020f7ad1fc327017ad3c429469dde13bb60572eb'
