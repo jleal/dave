f1fe493151bc64cb88a3fc0b8cf37b192cdbae9e
Juan Leal <sellingangle@hotmail.com>
2012-07-28 23:13:33 -0500

    Added a crud target for funy_books

---
+++ app/targets/funky_book_crud_target.rb
@@ -0,0 +1,54 @@
+
+class FunkyBookCrudTarget < Struct.new(:listener)
+
+  def index_for
+    funky_books = FunkyBook.all
+
+    listener.index_success(funky_books)
+  end
+
+  def show_for(funky_book_id)
+    funky_book = FunkyBook.find(funky_book_id)
+
+    listener.show_success(funky_book)
+  end
+
+  def new_for
+    funky_book = FunkyBook.new
+
+    listener.new_success(funky_book)
+  end
+
+  def edit_for(funky_book_id)
+    funky_book = FunkyBook.find(funky_book_id)
+
+    listener.edit_success(funky_book)
+  end
+
+  def create_for(funky_book_params)
+    funky_book = FunkyBook.new(funky_book_params)
+
+    if funky_book.save
+      listener.create_success(funky_book)      
+    else
+      listener.create_failed(funky_book)
+    end
+  end
+
+  def update_for(funky_book_id, funky_book_params)
+    funky_book = FunkyBook.find(funky_book_id)
+
+    if funky_book.update_attributes(funky_book_params)
+      listener.update_success(funky_book)      
+    else
+      listener.update_failed(funky_book)
+    end
+  end
+
+  def destroy_for(funky_book_id)
+    funky_book = FunkyBook.find(funky_book_id)
+    funky_book.destroy
+
+    listener.destroy_success(funky_book)
+  end
+end
