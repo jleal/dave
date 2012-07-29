1e917c8cd2eb67c1e6b8efc6808791f7c9d56e31
Juan Leal <sellingangle@hotmail.com>
2012-07-28 23:42:58 -0500

    Implementing controller delegates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  include BooksControllerDelegate
 
   #==> Actions
   def index
@@ -29,89 +30,4 @@ class BooksController < ApplicationController
     target.destroy_for(params[:id])
   end
 
-
-  #==> Callbacks
-  def index_success(books)
-    @books = books
-
-    respond_to do |format|
-      format.html { render 'index'}
-      format.json { render json: @books }
-    end   
-  end
-
-  def show_success(book)
-    @book = book
-
-    respond_to do |format|
-      format.html { render 'show'}
-      format.json { render json: @book }
-    end
-  end  
-
-  def new_success(book)
-    @book = book
-
-    respond_to do |format|
-      format.html { render 'new'}
-      format.json { render json: @book }
-    end
-  end  
-
-  def edit_success(book)
-    @book = book
-
-    respond_to {|format| render 'edit'}
-  end  
-
-  def create_success(book)
-    @book = book
-
-    respond_to do |format|
-      format.html { redirect_to @book, notice: 'Book was successfully created.' }
-      format.json { render json: @book, status: :created, location: @book }
-    end
-  end 
-
-  def create_failed(book)
-    @book = book
-
-    respond_to do |format|
-      format.html { render action: "new" }
-      format.json { render json: @book.errors, status: :unprocessable_entity }      
-    end
-  end   
-
-  def update_success(book)
-    @book = book
-
-    respond_to do |format|
-      format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-      format.json { head :no_content }      
-    end
-  end
-
-  def update_failed(book)
-    @book = book
-
-    respond_to do |format|
-      format.html { render action: "edit" }
-      format.json { render json: @book.errors, status: :unprocessable_entity }      
-    end
-  end    
-
-  def destroy_success(book)
-    @book = book
-
-    respond_to do |format|
-      format.html { redirect_to books_url }
-      format.json { head :no_content }
-    end
-  end  
-
-  private
-
-  def target
-    BookCrudTarget.new(self)
-  end
 end
--- app/controllers/funky_books_controller.rb
+++ app/controllers/funky_books_controller.rb
@@ -1,4 +1,5 @@
 class FunkyBooksController < ApplicationController
+  include FunkyBooksControllerDelegate
 
   #==> Actions
   def index
@@ -29,89 +30,4 @@ class FunkyBooksController < ApplicationController
     target.destroy_for(params[:id])
   end
 
-
-  #==> Callbacks
-  def index_success(funky_books)
-    @funky_books = funky_books
-
-    respond_to do |format|
-      format.html { render 'index'}
-      format.json { render json: @funky_books }
-    end   
-  end
-
-  def show_success(funky_book)
-    @funky_book = funky_book
-
-    respond_to do |format|
-      format.html { render 'show'}
-      format.json { render json: @funky_book }
-    end
-  end  
-
-  def new_success(funky_book)
-    @funky_book = funky_book
-
-    respond_to do |format|
-      format.html { render 'new'}
-      format.json { render json: @funky_book }
-    end
-  end  
-
-  def edit_success(funky_book)
-    @funky_book = funky_book
-
-    respond_to {|format| render 'edit'}
-  end  
-
-  def create_success(funky_book)
-    @funky_book = funky_book
-
-    respond_to do |format|
-      format.html { redirect_to @funky_book, notice: 'Book was successfully created.' }
-      format.json { render json: @funky_book, status: :created, location: @funky_book }
-    end
-  end 
-
-  def create_failed(funky_book)
-    @funky_book = funky_book
-
-    respond_to do |format|
-      format.html { render action: "new" }
-      format.json { render json: @funky_book.errors, status: :unprocessable_entity }      
-    end
-  end   
-
-  def update_success(funky_book)
-    @funky_book = funky_book
-
-    respond_to do |format|
-      format.html { redirect_to @funky_book, notice: 'Book was successfully updated.' }
-      format.json { head :no_content }      
-    end
-  end
-
-  def update_failed(funky_book)
-    @funky_book = funky_book
-
-    respond_to do |format|
-      format.html { render action: "edit" }
-      format.json { render json: @funky_book.errors, status: :unprocessable_entity }      
-    end
-  end    
-
-  def destroy_success(funky_book)
-    @funky_book = funky_book
-
-    respond_to do |format|
-      format.html { redirect_to funky_books_url }
-      format.json { head :no_content }
-    end
-  end  
-
-  private
-
-  def target
-    FunkyBookCrudTarget.new(self)
-  end
 end
