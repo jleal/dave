af1993ce24d32e7189f69167fe76f18d8f1ee727
Juan Leal <sellingangle@hotmail.com>
2012-07-29 01:01:08 -0500

    Modified delegates so they can be chained using ancestor chain

--- app/controller_delegates/books_controller_delegate.rb
+++ app/controller_delegates/books_controller_delegate.rb
@@ -2,87 +2,167 @@
 module BooksControllerDelegate
 
   #==> Target(s)
-  def target
-    BookCrudTarget.new(self)
+  def target(controller_type)
+
+    case 
+    when controller_type.to_sym == :books
+      BookCrudTarget.new(self)   
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end
 
   #==> Callbacks
-  def index_success(books)
-    @books = books
+  def index_success(books, controller_type)
+ 
+    case 
+    when controller_type.to_sym == :books
+      @books = books
 
-    respond_to do |format|
-      format.html { render 'index'}
-      format.json { render json: @books }
-    end   
+      respond_to do |format|
+        format.html { render 'index'}
+        format.json { render json: @books }
+      end          
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end    
   end
 
-  def show_success(book)
-    @book = book
+  def show_success(book, controller_type)
 
-    respond_to do |format|
-      format.html { render 'show'}
-      format.json { render json: @book }
-    end
+    case 
+    when controller_type.to_sym == :books
+      @book = book
+
+      respond_to do |format|
+        format.html { render 'show'}
+        format.json { render json: @book }
+      end        
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end  
 
-  def new_success(book)
-    @book = book
+  def new_success(book, controller_type)
 
-    respond_to do |format|
-      format.html { render 'new'}
-      format.json { render json: @book }
-    end
+    case 
+    when controller_type.to_sym == :books
+      @book = book
+
+      respond_to do |format|
+        format.html { render 'new'}
+        format.json { render json: @book }
+      end        
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end  
 
-  def edit_success(book)
-    @book = book
+  def edit_success(book, controller_type)
 
-    respond_to {|format| render 'edit'}
+    case 
+    when controller_type.to_sym == :books
+      @book = book
+
+      respond_to {|format| render 'edit'}        
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end  
 
-  def create_success(book)
-    @book = book
+  def create_success(book, controller_type)
 
-    respond_to do |format|
-      format.html { redirect_to @book, notice: 'Book was successfully created.' }
-      format.json { render json: @book, status: :created, location: @book }
-    end
+    case 
+    when controller_type.to_sym == :books
+      @book = book
+
+      respond_to do |format|
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render json: @book, status: :created, location: @book }
+      end        
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end 
 
-  def create_failed(book)
-    @book = book
+  def create_failed(book, controller_type)
 
-    respond_to do |format|
-      format.html { render action: "new" }
-      format.json { render json: @book.errors, status: :unprocessable_entity }      
-    end
+    case 
+    when controller_type.to_sym == :books
+      @book = book
+
+      respond_to do |format|
+        format.html { render action: "new" }
+        format.json { render json: @book.errors, status: :unprocessable_entity }      
+      end        
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end   
 
-  def update_success(book)
-    @book = book
+  def update_success(book, controller_type)
 
-    respond_to do |format|
-      format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-      format.json { head :no_content }      
-    end
+    case 
+    when controller_type.to_sym == :books
+      @book = book
+
+      respond_to do |format|
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { head :no_content }      
+      end        
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end
 
-  def update_failed(book)
-    @book = book
+  def update_failed(book, controller_type)
 
-    respond_to do |format|
-      format.html { render action: "edit" }
-      format.json { render json: @book.errors, status: :unprocessable_entity }      
-    end
+    case 
+    when controller_type.to_sym == :books
+      @book = book
+
+      respond_to do |format|
+        format.html { render action: "edit" }
+        format.json { render json: @book.errors, status: :unprocessable_entity }      
+      end        
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end    
 
-  def destroy_success(book)
-    @book = book
+  def destroy_success(book, controller_type)
 
-    respond_to do |format|
-      format.html { redirect_to books_url }
-      format.json { head :no_content }
-    end
+    case 
+    when controller_type.to_sym == :books
+      @book = book
+
+      respond_to do |format|
+        format.html { redirect_to books_url }
+        format.json { head :no_content }
+      end        
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end  
 
 end
--- app/controller_delegates/funky_books_controller_delegate.rb
+++ app/controller_delegates/funky_books_controller_delegate.rb
@@ -2,87 +2,167 @@
 module FunkyBooksControllerDelegate
 
   #==> Target(s)
-  def target
-    FunkyBookCrudTarget.new(self)
+  def target(controller_type)
+
+    case 
+    when controller_type.to_sym == :funky_books
+      FunkyBookCrudTarget.new(self)   
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end   
   end  
 
   #==> Callbacks
-  def index_success(funky_books)
-    @funky_books = funky_books
+  def index_success(funky_books, controller_type)
+ 
+    case 
+    when controller_type.to_sym == :funky_books
+      @funky_books = funky_books
 
-    respond_to do |format|
-      format.html { render 'index'}
-      format.json { render json: @funky_books }
-    end   
+      respond_to do |format|
+        format.html { render 'index'}
+        format.json { render json: @funky_books }
+      end        
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end
 
-  def show_success(funky_book)
-    @funky_book = funky_book
+  def show_success(funky_book, controller_type)
 
-    respond_to do |format|
-      format.html { render 'show'}
-      format.json { render json: @funky_book }
-    end
+    case 
+    when controller_type.to_sym == :funky_books
+      @funky_book = funky_book
+
+      respond_to do |format|
+        format.html { render 'show'}
+        format.json { render json: @funky_book }
+      end      
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end  
 
-  def new_success(funky_book)
-    @funky_book = funky_book
+  def new_success(funky_book, controller_type)
 
-    respond_to do |format|
-      format.html { render 'new'}
-      format.json { render json: @funky_book }
-    end
+    case 
+    when controller_type.to_sym == :funky_books
+      @funky_book = funky_book
+
+      respond_to do |format|
+        format.html { render 'new'}
+        format.json { render json: @funky_book }
+      end      
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end  
 
-  def edit_success(funky_book)
-    @funky_book = funky_book
+  def edit_success(funky_book, controller_type)
 
-    respond_to {|format| render 'edit'}
+    case 
+    when controller_type.to_sym == :funky_books
+      @funky_book = funky_book
+
+      respond_to {|format| render 'edit'}      
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end  
 
-  def create_success(funky_book)
-    @funky_book = funky_book
+  def create_success(funky_book, controller_type)
 
-    respond_to do |format|
-      format.html { redirect_to @funky_book, notice: 'Book was successfully created.' }
-      format.json { render json: @funky_book, status: :created, location: @funky_book }
-    end
+    case 
+    when controller_type.to_sym == :funky_books
+      @funky_book = funky_book
+
+      respond_to do |format|
+        format.html { redirect_to @funky_book, notice: 'Book was successfully created.' }
+        format.json { render json: @funky_book, status: :created, location: @funky_book }
+      end      
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end 
 
-  def create_failed(funky_book)
-    @funky_book = funky_book
+  def create_failed(funky_book, controller_type)
 
-    respond_to do |format|
-      format.html { render action: "new" }
-      format.json { render json: @funky_book.errors, status: :unprocessable_entity }      
-    end
+    case 
+    when controller_type.to_sym == :funky_books
+      @funky_book = funky_book
+
+      respond_to do |format|
+        format.html { render action: "new" }
+        format.json { render json: @funky_book.errors, status: :unprocessable_entity }      
+      end      
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end   
 
-  def update_success(funky_book)
-    @funky_book = funky_book
+  def update_success(funky_book, controller_type)
 
-    respond_to do |format|
-      format.html { redirect_to @funky_book, notice: 'Book was successfully updated.' }
-      format.json { head :no_content }      
-    end
+    case 
+    when controller_type.to_sym == :funky_books
+      @funky_book = funky_book
+
+      respond_to do |format|
+        format.html { redirect_to @funky_book, notice: 'Book was successfully updated.' }
+        format.json { head :no_content }      
+      end      
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end
 
-  def update_failed(funky_book)
-    @funky_book = funky_book
+  def update_failed(funky_book, controller_type)
 
-    respond_to do |format|
-      format.html { render action: "edit" }
-      format.json { render json: @funky_book.errors, status: :unprocessable_entity }      
-    end
+    case 
+    when controller_type.to_sym == :funky_books
+      @funky_book = funky_book
+
+      respond_to do |format|
+        format.html { render action: "edit" }
+        format.json { render json: @funky_book.errors, status: :unprocessable_entity }      
+      end      
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end    
 
-  def destroy_success(funky_book)
-    @funky_book = funky_book
+  def destroy_success(funky_book, controller_type)
 
-    respond_to do |format|
-      format.html { redirect_to funky_books_url }
-      format.json { head :no_content }
-    end
+    case 
+    when controller_type.to_sym == :funky_books
+      @funky_book = funky_book
+
+      respond_to do |format|
+        format.html { redirect_to funky_books_url }
+        format.json { head :no_content }
+      end      
+    when defined?(super)
+      super
+    else
+      raise NoMethodError, "undefined method #{__method__} in ancestor chain"
+    end      
   end  
 
 end
