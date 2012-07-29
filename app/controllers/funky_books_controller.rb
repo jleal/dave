64540bf5894b5df4b4ca4f0806f2d8d269af5fab
Juan Leal <sellingangle@hotmail.com>
2012-07-28 23:17:13 -0500

    Pulled out model interaction from controller

--- app/controllers/funky_books_controller.rb
+++ app/controllers/funky_books_controller.rb
@@ -1,83 +1,117 @@
 class FunkyBooksController < ApplicationController
-  # GET /funky_books
-  # GET /funky_books.json
+
+  #==> Actions
   def index
-    @funky_books = FunkyBook.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @funky_books }
-    end
+    target.index_for
   end
 
-  # GET /funky_books/1
-  # GET /funky_books/1.json
   def show
-    @funky_book = FunkyBook.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @funky_book }
-    end
+    target.show_for(params[:id])
   end
 
-  # GET /funky_books/new
-  # GET /funky_books/new.json
   def new
-    @funky_book = FunkyBook.new
+    target.new_for
+  end  
+
+  def edit
+    target.edit_for(params[:id])
+  end  
+
+  def create
+    target.create_for(params[:funky_book])
+  end  
+
+  def update
+    target.update_for(params[:id], params[:funky_book])
+  end  
+
+  def destroy
+    target.destroy_for(params[:id])
+  end
+
+
+  #==> Callbacks
+  def index_success(funky_books)
+    @funky_books = funky_books
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html { render 'index'}
+      format.json { render json: @funky_books }
+    end   
+  end
+
+  def show_success(funky_book)
+    @funky_book = funky_book
+
+    respond_to do |format|
+      format.html { render 'show'}
       format.json { render json: @funky_book }
     end
-  end
+  end  
 
-  # GET /funky_books/1/edit
-  def edit
-    @funky_book = FunkyBook.find(params[:id])
-  end
-
-  # POST /funky_books
-  # POST /funky_books.json
-  def create
-    @funky_book = FunkyBook.new(params[:funky_book])
+  def new_success(funky_book)
+    @funky_book = funky_book
 
     respond_to do |format|
-      if @funky_book.save
-        format.html { redirect_to @funky_book, notice: 'Funky book was successfully created.' }
-        format.json { render json: @funky_book, status: :created, location: @funky_book }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @funky_book.errors, status: :unprocessable_entity }
-      end
+      format.html { render 'new'}
+      format.json { render json: @funky_book }
+    end
+  end  
+
+  def edit_success(funky_book)
+    @funky_book = funky_book
+
+    respond_to {|format| render 'edit'}
+  end  
+
+  def create_success(funky_book)
+    @funky_book = funky_book
+
+    respond_to do |format|
+      format.html { redirect_to @funky_book, notice: 'Book was successfully created.' }
+      format.json { render json: @funky_book, status: :created, location: @funky_book }
+    end
+  end 
+
+  def create_failed(funky_book)
+    @funky_book = funky_book
+
+    respond_to do |format|
+      format.html { render action: "new" }
+      format.json { render json: @funky_book.errors, status: :unprocessable_entity }      
+    end
+  end   
+
+  def update_success(funky_book)
+    @funky_book = funky_book
+
+    respond_to do |format|
+      format.html { redirect_to @funky_book, notice: 'Book was successfully updated.' }
+      format.json { head :no_content }      
     end
   end
 
-  # PUT /funky_books/1
-  # PUT /funky_books/1.json
-  def update
-    @funky_book = FunkyBook.find(params[:id])
+  def update_failed(funky_book)
+    @funky_book = funky_book
 
     respond_to do |format|
-      if @funky_book.update_attributes(params[:funky_book])
-        format.html { redirect_to @funky_book, notice: 'Funky book was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @funky_book.errors, status: :unprocessable_entity }
-      end
+      format.html { render action: "edit" }
+      format.json { render json: @funky_book.errors, status: :unprocessable_entity }      
     end
-  end
+  end    
 
-  # DELETE /funky_books/1
-  # DELETE /funky_books/1.json
-  def destroy
-    @funky_book = FunkyBook.find(params[:id])
-    @funky_book.destroy
+  def destroy_success(funky_book)
+    @funky_book = funky_book
 
     respond_to do |format|
       format.html { redirect_to funky_books_url }
       format.json { head :no_content }
     end
+  end  
+
+  private
+
+  def target
+    FunkyBookCrudTarget.new(self)
   end
 end
